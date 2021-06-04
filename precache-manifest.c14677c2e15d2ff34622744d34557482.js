07a0643050cb57cbbec4b9562889e499f20f3f82
Abhay1612 <abhaytomar1612@gmail.com>
2021-06-04 23:03:41 +0530

    Updates

---
+++ precache-manifest.c14677c2e15d2ff34622744d34557482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14eb46486876d80d0ceff014995f2d8",
+    "url": "/React-Amazon/index.html"
+  },
+  {
+    "revision": "5c4dbfb264771244d58a",
+    "url": "/React-Amazon/static/css/main.4f160c4f.chunk.css"
+  },
+  {
+    "revision": "fa1ccc872c8d9438115c",
+    "url": "/React-Amazon/static/js/2.e5e76d0a.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/React-Amazon/static/js/2.e5e76d0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4dbfb264771244d58a",
+    "url": "/React-Amazon/static/js/main.4e45683a.chunk.js"
+  },
+  {
+    "revision": "e29cfa6c3457a8f02f29",
+    "url": "/React-Amazon/static/js/runtime-main.4dd8b66e.js"
+  }
+]);
